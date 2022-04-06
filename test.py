f515628e5c2aff58dc03ccde4d35012bfd0e31c7
rwbakerUMASS <rwbaker@umass.edu>
2022-04-06 17:12:42 -0400

    pyreplace testing

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
 from hx711 import HX711
+from firebase import Firebase
+import time
 
-hx = HX711(5,6)
+config = {
+  "apiKey": "AIzaSyBqmtDdXcCLp4ODEgtkelMj7QWEixxSVOY",
+  "authDomain": "intelli--bed.firebaseapp.com",
+  "databaseURL": "https://intelli--bed-default-rtdb.firebaseio.com/",
+  "storageBucket": "intelli--bed.appspot.com"
+}
+prevTime = time.time()
+data = {
+  "mean":4,
+  "stdev":34,
+  "numPeaks":2
+}
+fb = Firebase(config)
+fb.clearTable()
 
-hx.reset()
-hx.set_reference_unit()
-print(hx.get_weight())
+# hx = HX711(5,6)
+# hx.reset()
+# hx.set_reference_unit()
+# print(hx.get_weight())
