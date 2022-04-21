d1318fabbcc8ab422aad75478ae51b025ac4389d
rwbakerUMASS <rwbaker@umass.edu>
2022-04-20 23:51:12 -0400

    Update intellibed-system.ino

--- intellibed-system/intellibed-system.ino
+++ intellibed-system/intellibed-system.ino
@@ -12,15 +12,27 @@ char api[] = "AIzaSyBqmtDdXcCLp4ODEgtkelMj7QWEixxSVOY";
 FirebaseData fbdo;
 void setup() {
   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Starting Firebase");
   Firebase.begin(url, api, ssid, pass);
-  Firebase.reconnectWiFi(true);
+  Serial.println("Firebase started");
+  while (!testWiFiStatus()) {
+    delay(100);
+    Serial.println("WiFi Failed to Connect");
+    Serial.println("Retrying...");
+    Firebase.reconnectWiFi(true);
+  }
+  printWiFiStatus();
+  Serial.println("WiFi Started");
   setFbFloat(23232, 1111111111111111.1111);
 }
 void setFbFloat(float data, float timestamp) {
   String child = "/test/";
   char timeString[20];
-  dtostrf(timestamp,20,6,timeString);
-  if (Firebase.setFloat(fbdo, child+timeString, 123.456789)) {
+  dtostrf(timestamp, 20, 6, timeString);
+  if (Firebase.setFloat(fbdo, child + timeString, 123.456789)) {
 
     //Success, then read the payload value return from server
     //This confirmed that your data was set to database as float number
@@ -34,7 +46,28 @@ void setFbFloat(float data, float timestamp) {
   }
 }
 
-void loop(){
-  
+bool testWiFiStatus() {
+  return WiFi.status() == WL_CONNECTED;
+}
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi 101 Shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void loop() {
+
 }
 
